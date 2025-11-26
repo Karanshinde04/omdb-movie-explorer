937c30ed56b4601323144658da9968a53ae9137d
Karanshinde04 <139993175+Karanshinde04@users.noreply.github.com>
2025-11-26 18:30:53 +0530

    Add files via upload

---
+++ omdbexplorer/src/test/java/com/example/omdbexplorer/OmdbexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omdbexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdbexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
