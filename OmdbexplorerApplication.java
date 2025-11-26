a23c6cf99bb6e9c34ee7da56ac81ae06736f4639
Karanshinde04 <139993175+Karanshinde04@users.noreply.github.com>
2025-11-26 17:33:34 +0530

    Add files via upload

---
+++ OmdbexplorerApplication.java
@@ -0,0 +1,12 @@
+package com.example.omdbexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdbexplorerApplication.class, args);
+	}
+}
